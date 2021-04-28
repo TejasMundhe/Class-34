b13514cd8bf82deed09191c546838378d98d13e1
TJ907 <81124510+TJ907@users.noreply.github.com>
2021-04-27 20:54:14 -0400

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,22 @@
+class Ball{
+    constructor(x, y, r, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, r, options);
+        this.r = r;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos = this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        fill("gray");
+        ellipse(0, 0, this.r, this.r);
+        pop();
+      }
+}
---
+++ Chain.js
@@ -0,0 +1,30 @@
+class Chain{
+    constructor(bodyA, pointB) {
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1.2,
+            length: 200
+        }
+        this.pointB = pointB;
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+      }
+      fly(){
+          this.chain.bodyA = null;
+      }
+      attach(body){
+          this.chain.bodyA = body;
+      }
+
+      display(){
+        if(this.chain.bodyA){
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.pointB;
+        push();
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        pop();
+      }
+      }
+    }
